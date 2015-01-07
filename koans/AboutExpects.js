1fd373951efe335ba1b503af9001c3e68212c259
Sean Kim <sjkim2636@gmail.com>
2015-01-06 22:43:21 -0800

    Update AboutExpects.js

--- koans/AboutExpects.js
+++ koans/AboutExpects.js
@@ -7,8 +7,8 @@ describe("About Expects", function() {
 
   //To understand reality, we must compare our expectations against reality.
   it("should expect equality", function () { 
-	  var expectedValue = FILL_ME_IN;
-	  var actualValue = 1 + 1;
+	  var expectedValue = 2;
+	  var actualValue = (1 + 1);
 	
 	  expect(actualValue === expectedValue).toBeTruthy();
   });  
